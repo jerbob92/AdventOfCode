357a8f94aebb48c411021ea1f4dc274be8db635f
Jeroen Bobbeldijk <jerbob92@gmail.com>
2015-12-06 14:45:53 +0100

    Add day 4

---
+++ src/Day4/Day4Part1.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"crypto/md5"
+	"log"
+	"strings"
+	"encoding/hex"
+	"strconv"
+)
+
+func main() {
+	input := "iwrupvqb"
+	hasher := md5.New()
+	resultHash := ""
+	lowestNumber := -1
+	prefix := "00000"
+
+	// Create a new MD5 hash until we have one that starts with 00000
+	for !strings.HasPrefix(resultHash, prefix) {
+		// Reset the hashing machine.
+		hasher.Reset()
+
+		// Try the next number.
+		lowestNumber++;
+
+		// Generate the hash for the next number.
+		hasher.Write([]byte(input + strconv.Itoa(lowestNumber)))
+		resultHash = hex.EncodeToString(hasher.Sum(nil))
+	}
+
+	if (lowestNumber > -1) {
+		log.Printf("The lowest number that results in a hash that starts with %s with key %s + i is where i = %d", prefix, input, lowestNumber)
+	}
+}
