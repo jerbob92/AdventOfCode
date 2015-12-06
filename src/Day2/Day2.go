b6e38e306ce12b6e4ec25f709393ccfea31c64c2
Jeroen Bobbeldijk <jerbob92@gmail.com>
2015-12-06 14:09:39 +0100

    Add neededribbon

--- src/Day2/Day2.go
+++ src/Day2/Day2.go
@@ -17,6 +17,8 @@ func main() {
 
 	neededWrappingPaper := 0
 
+	neededFeetOfRibbon := 0
+
 	// We use a scanner to loop through every line.
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -30,6 +32,7 @@ func main() {
 		packageDimensionsStrings := strings.Split(packageDimension, "x")
 
 		// Convert the dimensions to ints
+		// No error checking as we are sure they are all ints.
 		packageDimensionsInts := map[int]int{}
 		packageDimensionsInts[0], _ = strconv.Atoi(packageDimensionsStrings[0])
 		packageDimensionsInts[1], _ = strconv.Atoi(packageDimensionsStrings[1])
@@ -59,7 +62,26 @@ func main() {
 
 		// Add the slack paper.
 		neededWrappingPaper += slackPaper
+
+		// Get the smallest combination of ribbon.
+		neededFeetOfRibbonForThisPackage := (packageDimensionsInts[0] + packageDimensionsInts[0] + packageDimensionsInts[1] + packageDimensionsInts[1])
+
+		if ((packageDimensionsInts[1] + packageDimensionsInts[1] + packageDimensionsInts[2] + packageDimensionsInts[2]) < neededFeetOfRibbonForThisPackage) {
+			neededFeetOfRibbonForThisPackage = (packageDimensionsInts[1] + packageDimensionsInts[1] + packageDimensionsInts[2] + packageDimensionsInts[2])
+		}
+
+		if ((packageDimensionsInts[0] + packageDimensionsInts[0] + packageDimensionsInts[2] + packageDimensionsInts[2]) < neededFeetOfRibbonForThisPackage) {
+			neededFeetOfRibbonForThisPackage = (packageDimensionsInts[0] + packageDimensionsInts[0] + packageDimensionsInts[2] + packageDimensionsInts[2])
+		}
+
+		// Calculate the needed ribbon to wrap this package
+		neededFeetOfRibbon += neededFeetOfRibbonForThisPackage
+
+		// Calculate the needed ribbon for the bow
+		// Ribbon bow calculation = l*w*h
+		neededFeetOfRibbon += (packageDimensionsInts[0] * packageDimensionsInts[1] * packageDimensionsInts[2])
 	}
 
-	log.Printf("We need to order %d quare feet of wrapping paper.", neededWrappingPaper)
+	log.Printf("Part 1: We need to order %d quare feet of wrapping paper.", neededWrappingPaper)
+	log.Printf("Part 2: We need to order %d feet of ribbon.", neededFeetOfRibbon)
 }
