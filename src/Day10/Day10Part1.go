5935939c372410be366aa5572c168680a49dd580
Jeroen Bobbeldijk <jerbob92@gmail.com>
2015-12-10 16:16:53 +0100

    Add day 10

---
+++ src/Day10/Day10Part1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+func main() {
+	input := "1321131112"
+	inputArray := []string{}
+
+	for _, char := range input {
+		inputArray = append(inputArray, string(char))
+	}
+
+	for pass := 1; pass <= 40; pass++ {
+		lastChar := ""
+		currentCharCount := 1
+		newString := []string{}
+		for i, char := range inputArray {
+			if (i == 0) {
+				lastChar = char
+				continue;
+			}
+			if (lastChar == char) {
+				currentCharCount++;
+			} else {
+				newString = append(newString, strconv.Itoa(currentCharCount))
+				newString = append(newString, lastChar)
+
+				lastChar = char
+				currentCharCount = 1
+			}
+		}
+		newString = append(newString, strconv.Itoa(currentCharCount))
+		newString = append(newString, lastChar)
+		inputArray = newString
+	}
+
+	log.Printf("The length of the resulting string is %d", len(inputArray))
+}
